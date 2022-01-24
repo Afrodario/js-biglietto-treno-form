ffa47fcba587771c697cae98d2b4bf7de7cfa767
Dario La Tegola <dariolategola@gmail.com>
2022-01-24 17:02:09 +0100

    Completati generatori random

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
-const passengerName = document.getElementById("input-name");
-const ticketName = document.getElementById("psg-name");
+let passengerName = document.getElementById("input-name");
+let ticketName = document.getElementById("psg-name");
 
-const generate = document.getElementById("generate");
+let kmNumber = document.getElementById("input-km");
+let ticketPrice = document.getElementById("psg-price");
+
+let ticketSeat = document.getElementById("psg-seat");
+let ticketCode = document.getElementById("psg-code");
+
+const pricePerKM = 0.21;
+
+let generate = document.getElementById("generate");
 
 generate.addEventListener("click",
     function() {
+
         ticketName.innerHTML = passengerName.value;
+
+        let totalPrice = (kmNumber.value * pricePerKM);
+        ticketPrice.innerHTML = totalPrice.toFixed(2) + "€";
+
+        let carNumber = Math.floor(Math.random() * 9) + 1;
+        ticketSeat.innerHTML = carNumber;
+
+        let codePR = Math.floor(Math.random() * (99999 - 90000 + 1) + 90000);
+        ticketCode.innerHTML = codePR;
     }
 )
